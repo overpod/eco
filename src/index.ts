23629512dacda23de1dc183ea59164adce8edd07
Trofimov Alex <overpod@yandex.ru>
2022-12-02 11:17:35 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,114 @@
-import {Sdk} from "@unique-nft/sdk";
-import {KeyringProvider} from "@unique-nft/accounts/keyring";
+import { Client, Sdk } from '@unique-nft/sdk';
+import { KeyringAccount, KeyringProvider } from '@unique-nft/accounts/keyring';
 
 import { createCanvas, loadImage, registerFont } from 'canvas';
 import axios from 'axios';
-import { writeFileSync } from 'fs';
+import { promises as fs } from 'fs';
 import { CronJob } from 'cron';
 
 const main = async () => {
-  ckeckEnv();
+  ckeckEnv([
+    'CHAIN_WS_URL',
+    'OWNER_ADDRESS',
+    'ADMIN_MNEMONIC',
+    'COLLECTION_ID',
+    'IPFS_URL',
+    'CRON_TIME',
+    'API_COUNT_URL',
+    'API_WEIGHT_URL',
+    'API_KEY',
+    'FILES_DIR',
+  ]);
 
+  const adminAccount = await KeyringProvider.fromMnemonic(process.env.ADMIN_MNEMONIC!!);
+  const sdk = new Sdk({
+    baseUrl: 'https://rest.dev.uniquenetwork.dev/v1',
+    signer: adminAccount,
+  });
+
+  const image = {
+    cid: 'QmdsyUhy3dJYVAhMGofsEKH6kEkinciXpMJcmhZ1XJiSW1',
+    fullUrl: 'https://ipfs.uniquenetwork.dev/ipfs/QmdsyUhy3dJYVAhMGofsEKH6kEkinciXpMJcmhZ1XJiSW1',
+    fileUrl: 'https://ipfs.uniquenetwork.dev/ipfs/QmdsyUhy3dJYVAhMGofsEKH6kEkinciXpMJcmhZ1XJiSW1',
+  };
+
+  //const collection = await createEcoCollection(sdk, adminAccount);
+  //console.log('Collection created', collection);
+
+  const collection = await sdk.collections.get({ collectionId: parseInt(process.env.COLLECTION_ID!!) });
+
+  
+  const createResult = await sdk.tokens.create.submitWaitResult({
+    owner: adminAccount.getAddress(),
+    data: {
+      image: {
+        ipfsCid: 'QmQbVM8zTcD6LS4vJ6eiygvQeSBaSvaf2pFTm6VpFGxx52',
+      },
+    },
+    collectionId: collection.id,
+    address: adminAccount.getAddress(),
+    properties: [
+      {
+        key: 'totalPlasticCount',
+        value: '0',
+      },
+      {
+        key: 'totalPlasticWeight',
+        value: '0',
+      },
+      {
+        key: 'totalAluminumCount',
+        value: '0',
+      },
+      {
+        key: 'totalAluminumWeight',
+        value: '0',
+      },
+    ],
+  });
+
+  console.log(createResult, '----');
+
+  if (createResult.isCompleted && createResult.parsed) {
+    const token = await sdk.tokens.get({
+      collectionId: parseInt(process.env.COLLECTION_ID!!),
+      tokenId: createResult.parsed.tokenId,
+    });
+    console.log(token, '----');
+  }
+
+
+  /*
+    const token = await sdk.tokens.create.submitWaitResult({
+      owner: process.env.OWNER_ADDRESS!!,
+      data: {
+        image: {
+          urlInfix: 'string',
+          hash: 'string',
+        },
+      },
+      collectionId: parseInt(process.env.COLLECTION_ID!!),
+      address: process.env.OWNER_ADDRESS!!,
+    });
+    */
+
+  // ---------------------------
+  /*
   const task = async () => {
     const data = await getData(process.env.API_COUNT_URL!!, process.env.API_WEIGHT_URL!!, process.env.API_KEY!!);
     if (!data) {
       return null;
     }
 
-    await generateAndSaveResultImage({
+    const file = await generateAndSaveResultImage({
       count: data.count,
       weight: data.weight,
       filesDirPath: `${__dirname}/${process.env.FILES_DIR!!}`,
     });
+
+    const response = await sdk.ipfs.uploadFile({ file });
+
+    console.log(response, '--------');
 
     return data;
   };
@@ -39,6 +128,7 @@ const main = async () => {
   );
 
   job.start();
+  */
 };
 
 const generateAndSaveResultImage = async ({
@@ -50,7 +140,7 @@ const generateAndSaveResultImage = async ({
   weight: number;
   filesDirPath: string;
 }) => {
-  const image = await loadImage(`${filesDirPath}/source.jpg`);
+  const image = await loadImage(`${filesDirPath}/source.png`);
 
   registerFont(`${filesDirPath}/font.ttf`, { family: 'Font' });
   const canvas = createCanvas(image.width, image.height);
@@ -65,7 +155,11 @@ const generateAndSaveResultImage = async ({
   ctx.fillText(count.toString(), 500, 1200);
   ctx.fillText(weight.toString(), 500, 1350);
 
-  writeFileSync(`${filesDirPath}/result.jpeg`, canvas.toBuffer('image/jpeg'));
+  const buffer = canvas.toBuffer('image/png');
+
+  //await fs.writeFile(`${filesDirPath}/result.png`, buffer);
+
+  return buffer;
 };
 
 const getData = async (apiCountUrl: string, apiWeightUrl: string, apiKey: string) => {
@@ -84,20 +178,82 @@ const getData = async (apiCountUrl: string, apiWeightUrl: string, apiKey: string
   if (!dataCount && !dataWeight) {
     return null;
   }
+  console.log(dataCount, dataWeight);
   // Тут непонятно что возвращать пластик или алюминий
   const count = dataCount?.totalPlasticCount as number;
   const weight = dataWeight?.totalPlasticWeight as number;
   return count && weight ? { count, weight } : null;
 };
 
-const envNames = ['CHAIN_WS_URL', 'OWNER_SEED', 'CRON_TIME', 'API_COUNT_URL', 'API_WEIGHT_URL', 'API_KEY', 'FILES_DIR'];
-
-const ckeckEnv = () => {
+const ckeckEnv = (envNames: string[]) => {
   envNames.forEach((envName) => {
     if (!process.env[envName]) {
       throw new Error(`${envName} is not set`);
     }
   });
 };
+
+const createEcoCollection = async (sdk: Client, adminAccount: KeyringAccount) =>
+  sdk.collections.creation.submitWaitResult({
+    mode: 'NFT',
+    name: 'Eco',
+    description: 'Eco',
+    tokenPrefix: 'ECO',
+    metaUpdatePermission: 'ItemOwner',
+    permissions: {
+      access: 'Normal',
+      mintMode: true,
+      nesting: {
+        tokenOwner: true,
+        collectionAdmin: true,
+      },
+    },
+    readOnly: true,
+    address: adminAccount.getAddress(),
+    tokenPropertyPermissions: [
+      {
+        key: 'totalPlasticCount',
+        permission: {
+          mutable: true,
+          collectionAdmin: true,
+          tokenOwner: true,
+        },
+      },
+      {
+        key: 'totalPlasticWeight',
+        permission: {
+          mutable: true,
+          collectionAdmin: true,
+          tokenOwner: true,
+        },
+      },
+      {
+        key: 'totalAluminumCount',
+        permission: {
+          mutable: true,
+          collectionAdmin: true,
+          tokenOwner: true,
+        },
+      },
+      {
+        key: 'totalAluminumWeight',
+        permission: {
+          mutable: true,
+          collectionAdmin: true,
+          tokenOwner: true,
+        },
+      },
+    ],
+    schema: {
+      coverPicture: {
+        ipfsCid: '',
+      },
+      image: {
+        urlTemplate: `${process.env.IPFS_URL}/ipfs/{infix}`,
+      },
+      schemaName: 'unique',
+      schemaVersion: '1.0.0',
+    },
+  });
 
 main();
